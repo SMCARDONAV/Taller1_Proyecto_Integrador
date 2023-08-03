a065a9822bdd2b7791954dee547e205921aa8e48
SMCARDONAV <92750321+SMCARDONAV@users.noreply.github.com>
2023-08-03 14:22:08 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,9 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html')
